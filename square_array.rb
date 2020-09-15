a2758dbb7e2cac10e56fba9400dcba7f0a2f3e92
thoughtful-storage-6584 <csimon1127@gmail.com>
2020-09-15 19:21:10 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,5 +2,6 @@ def square_array(array)
   # your code here
   new_array = []
   #array.append
-  array.each{| item |                                                                                                                            }
+  array.each{| item | new_array.append(item)}
+  new_array
 end
